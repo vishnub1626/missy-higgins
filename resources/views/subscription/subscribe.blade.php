1759ad785b813e9b787328882285d369544e9764
Vishnu B <vishnub1626@gmail.com>
2021-05-08 19:04:53 +0530

    Adds error text in subscription page

--- resources/views/subscription/subscribe.blade.php
+++ resources/views/subscription/subscribe.blade.php
@@ -8,13 +8,19 @@
             @csrf
             <div class="mb-4">
                 <input
-                    class="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    class="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @if ($errors->has('name')) border-red-500 @endif"
                     id="name" type="text" placeholder="Name" name="name" required>
+                @if ($errors->has('name'))
+                    <p class="text-red-500 text-xs italic mt-1">{{ $errors->first('name') }}</p>
+                @endif
             </div>
             <div class="mb-6">
                 <input
-                    class="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    class="shadow-sm appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @if ($errors->has('email')) border-red-500 @endif"
                     id="email" type="email" placeholder="Email" name="email" required>
+                @if ($errors->has('email'))
+                    <p class="text-red-500 text-xs italic mt-1">{{ $errors->first('email') }}</p>
+                @endif
             </div>
             <button
                 class="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-3 rounded focus:outline-none focus:shadow-outline"
