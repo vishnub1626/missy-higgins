66bea2bf31a26cf34cfabed0e5adf9c4a6cd5355
Vishnu B <vishnub1626@gmail.com>
2021-05-08 19:24:18 +0530

    Adds the error lables for create newsletter form

--- resources/views/admin/newsletter.blade.php
+++ resources/views/admin/newsletter.blade.php
@@ -22,17 +22,23 @@
                         Subject
                     </label>
                     <input
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline @if ($errors->has('subject')) border-red-500 @endif"
                         id="subject" type="text" placeholder="subject" name="subject">
+                    @if ($errors->has('subject'))
+                        <p class="text-red-500 text-xs italic mt-1">{{ $errors->first('subject') }}</p>
+                    @endif
                 </div>
                 <div class="mt-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="sending-to">
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="editor">
                         Body
                     </label>
                     <input id="editor" type="hidden" name="body">
                     <trix-editor input="editor"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 h-96 overflow-auto focus:outline-none focus:shadow-outline">
+                        class="shadow appearance-none border rounded w-full py-2 px-3 h-96 overflow-auto focus:outline-none focus:shadow-outline @if ($errors->has('body')) border-red-500 @endif">
                     </trix-editor>
+                    @if ($errors->has('body'))
+                        <p class="text-red-500 text-xs italic mt-1">{{ $errors->first('body') }}</p>
+                    @endif
                 </div>
                 <div class="flex items-center justify-end mt-4">
                     <a class="bg-gray-300 hover:bg-gray-400 text-black mr-5 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
